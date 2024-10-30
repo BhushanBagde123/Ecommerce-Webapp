acd84ad9bfd4e31b9fb0a27e9752ecfff3d78449
BhushanBagde123 <bhushnabagade7@gmail.com>
2024-10-30 12:44:51 +0530

    add remove cart functionality

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -1,20 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { ContexPrv } from '../Context/Context'
 
 const CartPage = () => {
-    const {cart} =useContext(ContexPrv)
+  
+    const {cart,setCart} =useContext(ContexPrv);
+    const removeCart =(id)=>{
+       const updateCart= cart.filter((crt)=>crt.id !== id);
+       setCart(updateCart)
+
+    }
   return (
     <div>
         {cart.length>0?(
             
             cart.map((item)=>{
                 return (
-                    <div key={item.id}>
-                        <img src={item.image} alt="" />
+                    <div key={item.id} className='flex'>
+                        <img className='w-40 h-40' src={item.image} alt="" />
+                        <h3>{item.title}</h3>
+                        <button onClick={()=>removeCart(item.id)}>remove</button>
                     </div>
                 )
             })
-        ):<div>no data found</div>}
+        ):<div>your cart is empty</div>}
     </div>
   )
 }
