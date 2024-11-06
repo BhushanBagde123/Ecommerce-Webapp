18e860742815068b791ec48718760123ce1ac66e
BhushanBagde123 <bhushnabagade7@gmail.com>
2024-11-06 13:42:58 +0530

    search optimization

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,8 +1,10 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, {  useEffect, useState } from 'react'
+import { useNavigate,Link } from 'react-router-dom'
 
 
 const Search = ({searchItem,input}) => {
     const [hide,setHide] =useState(false)
+    const navigate =useNavigate()
 
     useEffect(()=>{
         if(input ===""){
@@ -14,14 +16,23 @@ const Search = ({searchItem,input}) => {
         }
     },[input])
    
+    const navigationHide =(id)=>{
+       
+        
+        navigate(`/detail/${id}`)
+        setHide(true)
+       
+    }
+   
  
   return (
     !hide && (<div className={`bg-white z-20`}>
         
-        {searchItem.map((item)=>{
+        {searchItem.slice(0,9).map((item)=>{
             return (
                 <div key={item.id}>
-                     <p>{item.title}</p>
+                    
+                   <p onClick={()=>navigationHide(item.id)} className='cursor-pointer'>{item.title}</p>
                 </div>
 
                
